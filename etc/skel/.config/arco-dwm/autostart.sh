6a98fe3e2c28191af7ff0e681cf24be01bd68750
coritanie <coritanie@gmail.com>
2020-10-05 09:02:47 +0100

    Welcome app. enabled in autostart

--- etc/skel/.config/arco-dwm/autostart.sh
+++ etc/skel/.config/arco-dwm/autostart.sh
@@ -6,7 +6,7 @@ function run {
     $@&
   fi
 }
-#run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
+run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 #xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
