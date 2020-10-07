861efd7f35b4d8773dca79f44e36e1e74721e82e
Erik Dubois <erik.dubois@gmail.com>
2020-10-07 16:52:30 +0200

    xfxfce4-taskmanager

--- etc/skel/.config/arco-dwm/config.h
+++ etc/skel/.config/arco-dwm/config.h
@@ -73,7 +73,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *filecmd[]  = { "thunar", NULL };
 static const char *calendar[]  = { "gsimplecal", NULL };
-static const char *bashtop[]  = { "urxvt 'bashtop' -e bashtop", NULL };
+static const char *bashtop[]  = { "xfce4-taskmanager", NULL };
 
 #include "selfrestart.c"
 
