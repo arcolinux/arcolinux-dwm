01a9074f91dae4968a7d547eb7381a39ed4b2e67
Erik Dubois <erik.dubois@gmail.com>
2020-10-07 16:36:40 +0200

    chchanging keys

--- git-v1+.sh
+++ git-v1+.sh
@@ -16,6 +16,7 @@
 # checking if I have the latest files from github
 echo "Checking for newer files online first"
 git pull
+echo "Copy config.h"
 cp etc/skel/.config/arco-dwm/config.h etc/skel/.config/arco-dwm/config.def.h
 ./make-package.sh
 # Below command will backup everything inside the project folder
