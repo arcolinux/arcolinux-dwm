5cea03f1594b44cf5eaf3b5d479ab4c4659cab85
Erik Dubois <erik.dubois@gmail.com>
2020-10-07 17:59:50 +0200

    adding xrandr for dual screen

--- etc/skel/.config/arco-dwm/autostart.sh
+++ etc/skel/.config/arco-dwm/autostart.sh
@@ -9,7 +9,13 @@ function run {
 run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
 #run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
 #run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
-#xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+#run xrandr --output eDP-1 --primary --mode 1368x768 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off
+#run xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#run xrandr --output DVI-I-0 --right-of HDMI-0 --auto
+#run xrandr --output DVI-1 --right-of DVI-0 --auto
+#run xrandr --output DVI-D-1 --right-of DVI-I-1 --auto
+#run xrandr --output HDMI2 --right-of HDMI1 --auto
+
 run "nm-applet"
 run "pamac-tray"
 run "variety"
