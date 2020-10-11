9e5a3fa4f6073b3346d74630c4c5116bdd83303a
Erik Dubois <erik.dubois@gmail.com>
2020-10-11 14:17:32 +0200

    adding keyboard shortcuts for Azerty and dual screen

--- etc/skel/.config/arco-dwm/config.h
+++ etc/skel/.config/arco-dwm/config.h
@@ -117,6 +117,30 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 };
 
+/* IF YOU HAVE A AZERTY KEYBOARD USE THESE CODES
+	TAGKEYS(                        XK_ampersand,              0)
+	TAGKEYS(                        XK_eacute,                 1)
+	TAGKEYS(                        XK_quotedbl,               2)
+	TAGKEYS(                        XK_apostrophe,             3)
+	TAGKEYS(                        XK_parenleft,              4)
+	TAGKEYS(                        XK_section,                5)
+	TAGKEYS(                        XK_egrave,                 6)
+	TAGKEYS(                        XK_exclam,                 7)
+	TAGKEYS(                        XK_ccedilla,               8)
+*/
+
+/* THESE ARE THE ORIGINAL QWERTY KEYBOARD CODES
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+*/
+
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
