efd85388b31f1a92e7bd16adf77aa6279d11099f
Erik Dubois <erik.dubois@gmail.com>
2021-03-13 09:53:45 +0100

    calamares tool

--- etc/skel/.config/arco-dwm/config.def.h
+++ etc/skel/.config/arco-dwm/config.def.h
@@ -50,6 +50,7 @@ static const Rule rules[] = {
 	{ "Xfce4-terminal",            NULL,       NULL,       0,            1,           -1 },
 	{ "firefox",                   NULL,       NULL,       0,            0,           -1 },
 	{ "Arcolinux-welcome-app.py",  NULL,       NULL,       0,            1,           -1 },
+	{ "Arcolinux-calamares-tool.py",  NULL,       NULL,       0,            1,           -1 },	
 };
 
 /* layout(s) */
