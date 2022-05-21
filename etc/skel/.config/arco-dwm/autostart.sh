60307b5bf3a81cb464d7fe2dce387321d2f5486c
arcolinuxz <arcolinuxinfo@gmail.com>
2022-05-21 07:40:58 +0200

    wallpaper

--- etc/skel/.config/arco-dwm/autostart.sh
+++ etc/skel/.config/arco-dwm/autostart.sh
@@ -33,7 +33,8 @@ sxhkd -c ~/.config/arco-dwm/sxhkd/sxhkdrc &
 run "conky -c $HOME/.config/arco-dwm/system-overview"
 #you can set wallpapers in themes as well
 feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-
+#wallpaper for other Arch based systems
+#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 #run applications from startup
 
 #run "insync start"
