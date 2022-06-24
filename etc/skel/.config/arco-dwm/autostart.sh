a1cb95431bee49c5d9aa67529bf3cb0ff3203cd0
arcolinuxz <arcolinuxinfo@gmail.com>
2022-06-24 10:47:34 +0200

    wallpaper

--- etc/skel/.config/arco-dwm/autostart.sh
+++ etc/skel/.config/arco-dwm/autostart.sh
@@ -32,6 +32,7 @@ sxhkd -c ~/.config/arco-dwm/sxhkd/sxhkdrc &
 #run "nitrogen --restore"
 run "conky -c $HOME/.config/arco-dwm/system-overview"
 #you can set wallpapers in themes as well
+feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
 feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 #wallpaper for other Arch based systems
 #feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
